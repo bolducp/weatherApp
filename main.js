9c9cd0b185914c2fbece7307cedb2109a72ff19e
Paige Bolduc <paigebolduc@gmail.com>
2016-01-16 01:43:13 -0800

    full working version

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ $(document).ready(init);
 
 var zipcode, cityState, feelsLike, weather, humidity, dateTime, currentIcon;
 
-
 function init(){
   getLocalWeather();
   $("#weatherSearch").click(getForecastDetails);
 }
 
+$('input').keypress(function (event) {
+   var key = event.which;
+   if (key == 13) {
+      getForecastDetails();
+    }
+});
 
 function getLocalWeather(callback){
   $.ajax({
@@ -23,7 +28,6 @@ function getLocalWeather(callback){
       alert(err);
     }
   })
-
 }
 
 function insertZipcode(data){
@@ -31,12 +35,10 @@ function insertZipcode(data){
   $("#location").val(zipcode);
 }
 
-
 function getForecastDetails(data){
   zipcode = $("#location").val();
   $.ajax({
     url: "http://api.wunderground.com/api/d8483e016960a875/conditions/q/" + zipcode + ".json",
-    //url: "http://api.wunderground.com/api/d8483e016960a875/hourly10day/q/CA/San_Francisco.json",
     type: "GET",
     success: function(data){
       parseWeatherData(data);
@@ -48,7 +50,6 @@ function getForecastDetails(data){
 
   $.ajax({
     url: "http://api.wunderground.com/api/d8483e016960a875/forecast/q/" + zipcode + ".json",
-    //url: "http://api.wunderground.com/api/d8483e016960a875/hourly10day/q/CA/San_Francisco.json",
     type: "GET",
     success: function(data){
       parseForecastData(data);
@@ -63,20 +64,6 @@ function parseForecastData(data){
   oneDayForecast(data, 1);
   oneDayForecast(data, 2);
   oneDayForecast(data, 3);
-  // console.log("HEEYYYYdata!!!", data);
-  // dateTime = moment(data.current_observation.local_time_rfc822).format('MMMM Do YYYY,   h:mma');
-  // cityState = data.current_observation.display_location.full;
-  // feelsLike = data.current_observation.feelslike_string;
-  // weather = data.current_observation.weather;
-  // currentIcon = data.current_observation.icon_url;
-  // humidity = data.current_observation.relative_humidity;
-  //
-  // $('#currentWeather h2').text("Current Weather in " + cityState);
-  // $('h3').text(dateTime);
-  // $('#weather p').text(weather);
-  // $('img').attr('src', currentIcon);
-  // $('#temp').text(feelsLike);
-  // $("#humidity").text(humidity);
 }
 
 function oneDayForecast(data, day){
@@ -88,30 +75,14 @@ function oneDayForecast(data, day){
   var lowC = data.forecast.simpleforecast.forecastday[day].low.celsius;
   var icon = data.forecast.simpleforecast.forecastday[day].icon_url;
 
-  $("#day" + day).prepend($("<p>").text(weather));
-  $("#day" + day).prepend($("<p>").text(dayOfWeek).addClass("dayOfWeek"));
-  var $day = $("#day" + day);
-  $('#day' + day + ' img').attr("src", icon);
-
-  var $high = $("<p>").text("High: " + highF + "F/ " + highC +"C").addClass("temps");
-  var $low = $("<p>").text("Low: " + lowF + "F/ " + lowC + "C").addClass("temps");
-  $("#day" + day).append($high);
-  $("#day" + day).append($low);
-
-
-
-
-
+  $("#day" + day + " .dayOfWeek").text(dayOfWeek);
+  $("#day" + day + " .typeWeather").text(weather);
+  $('#day' + day + ' img').attr("src", icon).addClass("animated infinite swing");
+  $("#day" + day + " .high").text("High: " + highF + "F/ " + highC +"C");
+  $("#day" + day + " .low").text("Low: " + lowF + "F/ " + lowC + "C");
 }
 
-
-
-
-
-
-
 function parseWeatherData(data){
-  console.log("data!!!", data);
   dateTime = moment(data.current_observation.local_time_rfc822).format('MMMM Do YYYY,   h:mma');
   cityState = data.current_observation.display_location.full;
   feelsLike = data.current_observation.feelslike_string;
@@ -122,44 +93,7 @@ function parseWeatherData(data){
   $('#currentWeather h2').text("Current Weather in " + cityState);
   $('h3').text(dateTime);
   $('#weather p').text(weather);
-  $('#weather img').attr('src', currentIcon);
+  $('#weather img').attr('src', currentIcon).addClass("animated infinite swing");
   $('#temp').text(feelsLike);
   $("#humidity").text(humidity);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// $.ajax({
-//   url: "http://api.wunderground.com/api/d8483e016960a875/conditions/q/CA/San_Francisco.json",
-//   type: "GET",
-//   success: function(data){
-//     parseResults(data);
-//   },
-//   error: function(err){
-//     alert(err);
-//   }
-// })
-//   function parseResults(data){
-//     console.log(data);
-//     // var $weather = $('<div>').text();
-//     // $("#weather").append($poke);
-//     }
-//
-// }
-
-
-
-
-
-///http://api.wunderground.com/api/d8483e016960a875/
